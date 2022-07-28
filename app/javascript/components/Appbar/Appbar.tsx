340b56f6313102c513aadbe2e7c74fda4b6e3aff
João Geonizeli <joao.geonizeli@gmail.com>
2022-07-27 22:35:15 -0300

    fix logo render on production

--- app/javascript/components/Appbar/Appbar.tsx
+++ app/javascript/components/Appbar/Appbar.tsx
@@ -208,12 +208,12 @@ const Logo: FC = () => (
     <img
       alt="Símbolo do Unifeso"
       className="hidden md:block h-12 w-auto"
-      src={unifesoLogo}
+      src={'https://www.unifeso.edu.br/images/logo/UNIFESO-BRANCO.png'}
     />
     <img
       alt="Logotipo do Unifeso"
-      className="md:hidden h-12 w-auto"
-      src={unifesoLogoCompact}
+      className="md:hidden h-12 w-12 object-cover object-left"
+      src={'https://www.unifeso.edu.br/images/logo/UNIFESO-BRANCO.png'}
     />
   </div>
 )
